50a4dfbbef29e7a840328ff7277d664f72cebb61
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-02-03 22:37:03 +0800

    fix: import order follow alphabetical order

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,6 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
+import styles from '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Link from 'next/link';
