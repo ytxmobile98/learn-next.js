9e8c77c7dbf6f6e7fa13c865ff231400734d98fc
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2025-12-02 17:03:25 +0800

    fix: invalid_type_error

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -10,13 +10,13 @@ import { z } from 'zod';
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
-    invalid_type_error: 'Please select a customer.',
+    error: 'Please select a customer.',
   }),
   amount: z.coerce
     .number()
     .gt(0, { message: 'Please enter an amount greater than $0.' }),
   status: z.enum(['pending', 'paid'], {
-    invalid_type_error: 'Please select an invoice status.',
+    error: 'Please select an invoice status.',
   }),
   date: z.string(),
 });
